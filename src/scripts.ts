c642c0778a0dc40ff1cf2c2d5c1ee4845bac6e68
Lininger, Kurtis Wayne <KLININGER@bethel.jw.org>
2026-04-04 21:47:17 -0400

    Better url

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,7 +1,15 @@
+const isLocal = 
+    window.location.hostname === "127.0.0.1" ||
+    window.location.hostname === "localhost";
+
+const apiBase = isLocal
+  ? "http://localhost:7071/api"
+  : "/api";   // Static Web Apps automatically routes this to Azure Functions
+
 async function getData(url:string, destinationdiv: string) {
     var el = document.getElementById(destinationdiv);
     if (el !== null) {
-        fetch(url)
+        fetch(`${apiBase}/${url}`)
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
